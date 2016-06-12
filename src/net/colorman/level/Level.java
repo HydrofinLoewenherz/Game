423b55d8df5079f5e83cfbfec9b23aa394e7af85
Hauke Sommerfeld <haukesomm@gmail.com>
2016-06-12 22:28:14 +0200

    Added background resource loading to Level

--- src/net/colorman/level/Level.java
+++ src/net/colorman/level/Level.java
@@ -1,7 +1,10 @@
 package net.colorman.level;
 
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import net.colorman.resources.ResourceLoader;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -9,15 +12,23 @@ import java.util.List;
  */
 public abstract class Level {
 
+    private final ResourceLoader resourceLoader;
+
     private final int[] backgroundSpan;
-    private final List<ImageView> backgroundArray;
+    private List<Image> backgroundArray;
 
     public Level(int[] backgroundSpan) {
+        this.resourceLoader = new ResourceLoader();
         this.backgroundSpan = backgroundSpan;
         loadResources();
     }
 
     private final void loadResources() {
+        try {
+            backgroundArray = resourceLoader.getBackgroundImages(backgroundSpan[0], backgroundSpan[1]);
+        } catch (IOException e) {
+            e.printStackTrace(); // Error Handling muss hinzugefügt werden!
+        }
     }
 
 }
