a3e20c64c42e0d9745e2c3f1d25746f808bc2124
Hauke Sommerfeld <haukesomm@gmail.com>
2016-06-12 22:27:30 +0200

    Added image loading functionality to ResourceLoader

--- src/net/colorman/resources/ResourceLoader.java
+++ src/net/colorman/resources/ResourceLoader.java
@@ -1,27 +1,69 @@
 package net.colorman.resources;
 
-import javafx.scene.image.Image;
+import javafx.embed.swing.SwingFXUtils;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by Paul on 12.06.2016.
- */
 public class ResourceLoader {
 
-    private static final String RESOURCE_PATH = "/net/colorman/resources";
+    private static final String RESOURCE_PATH = "/net/colorman/resources/";
 
-    public Image getImage(String name) {
+    private static final String IMAGES_FOLDER = "images/";
+    private static final String IMAGES_RESOURCE_PATH = RESOURCE_PATH + IMAGES_FOLDER;
+
+    /*
+    Background file name pattern:
+    "background[INDEX].png"
+    Example: "background0.png", "background1.png", ..., "background42.png", etc.
+    */
+    private static final String BACKGROUND_IMAGE_PREFIX = "background";
+    private static final String BACKGROUND_IMAGE_POSTFIX = ".png";
+
+    /*
+    Helper method to convert AWT into JavaFX Images
+     */
+    private javafx.scene.image.Image convertAWTImageToFX(java.awt.Image awtImage) {
+
+        BufferedImage bufferedImage = new BufferedImage(
+                awtImage.getHeight(null),
+                awtImage.getWidth(null),
+                BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D graphics2D = bufferedImage.createGraphics();
+        graphics2D.drawImage(awtImage, 0, 0, null);
+        graphics2D.dispose();
+
+        return SwingFXUtils.toFXImage(bufferedImage, null);
 
     }
 
-    public List<Image> getBackgrounds(int start, int end) {
-        List<Image> backgroundImages = new ArrayList<>();
+    public javafx.scene.image.Image getImage(String name) {
 
-        // for -Schleife
+        String imagePath = IMAGES_RESOURCE_PATH + name;
+        java.awt.Image awtImage = new ImageIcon(getClass().getResource(imagePath)).getImage();
+        return convertAWTImageToFX(awtImage);
 
+    }
+
+    public List<javafx.scene.image.Image> getBackgroundImages(int begin, int end) throws IOException {
+
+        List<javafx.scene.image.Image> backgroundImages = new ArrayList<>();
+
+        for (int index = begin; index < end; index++) {
+
+            String currentImagePath = IMAGES_RESOURCE_PATH + BACKGROUND_IMAGE_PREFIX + String.valueOf(index) + BACKGROUND_IMAGE_POSTFIX;
+
+            java.awt.Image awtImage = new ImageIcon(getClass().getResource(currentImagePath)).getImage();
+            backgroundImages.add(convertAWTImageToFX(awtImage));
+
+        }
 
         return backgroundImages;
+
     }
 }
