edcf6f557f83bf386c749bb64f30fd8b70d22652
Hauke Sommerfeld <haukesomm@gmail.com>
2016-06-12 22:35:58 +0200

    Further optimized imports of ResourceLoader

--- src/net/colorman/resources/ResourceLoader.java
+++ src/net/colorman/resources/ResourceLoader.java
@@ -1,12 +1,11 @@
 package net.colorman.resources;
 
-import javafx.embed.swing.SwingFXUtils;
-
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import javafx.embed.swing.SwingFXUtils;
 import javax.swing.ImageIcon;
 
 public class ResourceLoader {
