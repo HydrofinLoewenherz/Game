3fcf7ded2bbff54742baaa35ed58619fdb003175
Hydrofin <hydrofin@gmail.com>
2016-06-14 18:59:43 +0200

    abstact Vector Class for Movement

---
+++ src/net/colorman/misc/Vector.java
@@ -0,0 +1,35 @@
+package net.colorman.misc;
+
+/**
+ * Created by Paul on 14.06.2016.
+ */
+public abstract class Vector {
+
+    private double lengthX, lengthY, length;
+
+    public Vector(double dX, double dY, double length) {
+        lengthX = dX;
+        lengthY = dY;
+        setLength(length);
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+        double m = Math.sqrt(Math.pow(length, 2) / (Math.pow(lengthX, 2) + Math.pow(lengthY, 2)));
+        if (Double.isInfinite(m)) m = 0;
+        lengthX *= m;
+        lengthY *= m;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getLengthX() {
+        return lengthX;
+    }
+
+    public double getLengthY() {
+        return lengthY;
+    }
+}
